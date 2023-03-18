2f588a22c40a19a7dc1fa0a4b8a3efa5464c3601
Sunsun Kasajoo <st122283@ait.asia>
2023-03-18 23:23:12 +0700

    comments added

--- backend_base64/src/api.py
+++ backend_base64/src/api.py
@@ -1,9 +1,25 @@
+"""
+Author: Sunsun Kasajoo
+Date: March 18, 2023
+
+This module contains apis.
+"""
 from fastapi import FastAPI, Request
 import base64
 
 app = FastAPI()
 
+
 @app.get("/convert_to_base64")
-def convert_to_base64(term):
+def convert_to_base64(term: str):
+    """
+    This function converts the input string to base64 encoded string.
+
+    Parameters:
+    term(str): The file passed to the API.
+
+    Returns:
+    string: base64 encoded string.
+    """
     base64_bytes = base64.b64encode(bytes(term, 'utf-8'))
     return {"base64": base64_bytes.decode('utf-8'), "message": "Succefully converted the string"}
--- backend_main/src/api.py
+++ backend_main/src/api.py
@@ -1,3 +1,10 @@
+"""
+Author: Sunsun Kasajoo
+Date: March 18, 2023
+
+This module contains apis.
+"""
+
 from fastapi import FastAPI, Request, UploadFile
 import requests
 from PIL import Image, ImageDraw
@@ -7,16 +14,24 @@ import base64
 app = FastAPI()
 
 
-#Task 1
+# Task 1
 @app.post("/convert-to-csv")
-async def convert_to_csv(request: Request):
-    form_data = await request.form()
-    tsv_file = form_data.get("file")
+async def convert_to_csv(file: UploadFile):
 
-    if not tsv_file.filename.endswith(".tsv"):
+    """
+    This function converts the input tsv file and return array of csv lines.
+
+    Parameters:
+    file (UploadFile): The file passed to the API.
+
+    Returns:
+    array: The array containing csv lines.
+    """
+
+    if not file.filename.endswith(".tsv"):
         return {"message": "Invalid file format. Please pass TSV file only."}
 
-    contents = await tsv_file.read()
+    contents = await file.read()
     lines = contents.decode("utf-8").split("\n")
 
     csv_lines = []
@@ -28,19 +43,35 @@ async def convert_to_csv(request: Request):
     print(csv_lines)
     return {"data": csv_lines, "message": "File converted successfully"}
 
-#Task 2
+
+# Task 2
 @app.post("/bounding-box")
-async def draw_bounding_box(image_file: UploadFile, x_topleft: float, y_topleft : float, width: float, height: float):
+async def draw_bounding_box(image_file: UploadFile, x_topleft: float, y_topleft: float, width: float, height: float):
+
+    """
+    This function takes image as input and draws bounding box according to the data provided.
+
+    Parameters:
+    image_file (UploadFile): The image passed to the api.
+    x_topleft: coordinate of x axis start point.
+    y_topleft: coordinate of y axis start point.
+    width: width of the bounding box.
+    height: height of the bounding box.
+
+    Returns:
+    string: base64 encode string of image.
+    """
 
     image_bytes = await image_file.read()
     img = Image.open(io.BytesIO(image_bytes)).convert("RGBA")
-       
+
     if x_topleft + width < img.width or y_topleft + height < img.height:
         return "Invalid bounding box"
-    
+
     draw = ImageDraw.Draw(img)
-    
-    draw.rectangle((x_topleft, y_topleft, x_topleft + width, y_topleft + height), outline="red")
+
+    draw.rectangle((x_topleft, y_topleft, x_topleft + width,
+                   y_topleft + height), outline="red")
 
     img_byte_arr = io.BytesIO()
     img.save(img_byte_arr, format="PNG")
@@ -48,12 +79,23 @@ async def draw_bounding_box(image_file: UploadFile, x_topleft: float, y_topleft 
 
     return {"image": img_str, "message": "Successfully drawn a bounding box"}
 
-#Task 3
+
+# Task 3
 @app.get("/name-to-base64")
 def name_to_base64(name: str):
+    """
+    This function takes name as input and checks if it is 'Sunsun' and calls api in another container.
+
+    Parameters:
+    name (str): The name field passes as query param.
+
+    Returns:
+    string: base64 encode string of name.
+    """
     if (name.lower() == "sunsun"):
         try:
-            response = requests.get('http://backend_base64:8000/convert_to_base64?term=' + name.lower())
+            response = requests.get(
+                'http://backend_base64:8000/convert_to_base64?term=' + name.lower())
             return response.json()
         except Exception as e:
             return {"error": f"Error calling API: {str(e)}"}
